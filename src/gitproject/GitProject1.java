138b7a6d4dd2134cc1bb389e8b6466e92e87b1e7
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-26 01:13:28 +0530

    my first commit

---
+++ src/gitproject/GitProject1.java
@@ -0,0 +1,8 @@
+package gitproject;
+
+public class GitProject1 {
+    //This is my First GIT Project
+    public static void main(String[] args) {
+        System.out.println("This is my first GIT Project");
+    }
+}
