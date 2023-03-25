35c41fb7ac811a7f10fdf3a011bab43c66299f33
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-26 02:23:22 +0530

    second comit

--- src/gitproject/GitProject1.java
+++ src/gitproject/GitProject1.java
@@ -4,5 +4,6 @@ public class GitProject1 {
     //This is my First GIT Project
     public static void main(String[] args) {
         System.out.println("This is my first GIT Project");
+        System.out.println("This is a Git text project");
     }
 }
